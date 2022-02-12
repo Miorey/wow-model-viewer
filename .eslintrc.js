9dd53465b5ffc609c19adf420643bb188e0bb21e
Clement Balea <clem.balea@gmail.com>
2022-02-12 09:51:15 +0100

    fix animation list

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
     env: {
         "browser": true,
         "node": true,
-        "mocha": true
+        "mocha": true,
+        "es6": true
     },
     extends: [
         `eslint:recommended`
