4a211119c56eb395cfc087bfe3d1d8b08ba9d28b
Clement Balea <clem.balea@gmail.com>
2023-10-18 22:55:20 +0300

    fix WOTLK_TO_RETAIL_DISPLAY_ID_API (#44)
    * fix WOTLK_TO_RETAIL_DISPLAY_ID_API
    
    * coverage
    
    * remove useless step

---
+++ tests/setup.test.js
@@ -0,0 +1,14 @@
+const {describe, it} = require(`@jest/globals`)
+
+// Import the module to be tested
+import {WH} from '../setup'
+
+describe(`WH`, () => {
+    it(`check consts`, async () => {
+        expect(window.WOTLK_TO_RETAIL_DISPLAY_ID_API).toEqual(`https://wotlk.murlocvillage.com/api/items`)
+        expect(window.CONTENT_PATH).toEqual(`http://localhost:3001/modelviewer/live/`)
+    })
+    it(`check consts WH.debug`, async () => {
+        WH.debug(`Hello`, `world`)
+    })
+})
