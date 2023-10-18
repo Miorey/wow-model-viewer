4a6d85a15ae21db565dfc21639368b659113663f
clem <clem.balea@gmail.com>
2023-10-18 22:42:04 +0300

    fix WOTLK_TO_RETAIL_DISPLAY_ID_API

--- setup.js
+++ setup.js
@@ -2,7 +2,7 @@ if(!window.CONTENT_PATH){
     window.CONTENT_PATH =`http://localhost:3001/modelviewer/live/`
 }
 if(!window.WOTLK_TO_RETAIL_DISPLAY_ID_API){
-    window.WOTLK_TO_RETAIL_DISPLAY_ID_API=`https://wotlk.murlocvillage.com/`
+    window.WOTLK_TO_RETAIL_DISPLAY_ID_API=`https://wotlk.murlocvillage.com/api/items`
 }
 
 
